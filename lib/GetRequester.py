923566e53891fcccfee1bcae15b566be8790e757
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-21 12:44:25 +0300

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return (response.content)
 
     def load_json(self):
-        pass
+        people = json.loads(self.get_response_body())
+        people_list = []
+        for person in people:
+            people_list.append(person)
+
+        return people_list
